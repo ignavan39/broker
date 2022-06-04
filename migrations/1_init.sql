e8f74d6de977bcc5881059ff892bbfc53a3c6d1f
ignavan39 <ivanignatenko28@gmail.com>
2022-06-04 10:56:15 +0300

    fix migrations order

--- migrations/1_init.sql
+++ migrations/1_init.sql
@@ -9,29 +9,23 @@ CREATE TABLE users (
 
 create unique index user_email_idx on users(email);
 
-CREATE TABLE messages (
-    created_at timestamp not null default NOW(),
-    id uuid not null primary key,
-    text text,
-    forwards jsonb default '[]',
-    images jsonb default '[]',
-    sender_id uuid not null constraint user_id references users(id),
-    chat_id uuid not null constraint chat_id references chats(id)
-);
-
 CREATE TABLE chats (
     id uuid not null primary key,
     created_at timestamp not null default NOW()
 );
 
 CREATE TABLE user_chats (
-    id uuid not null primary key,
+    id uuid not null default uuid_generate_v4() primary key,
     user_id uuid not null constraint user_chat_id references users(id),
     chat_id uuid not null constraint chat_user_id references chats(id),
     is_blocked boolean default false
 );
-CREATE TABLE user_chats (
-    id uuid not null primary key,
-    user_id uuid not null constraint user_chat_id references users(id),
-    chat_id uuid not null constraint chat_user_id references chats(id),
+CREATE TABLE messages (
+    created_at timestamp not null default NOW(),
+    id uuid not null default uuid_generate_v4() primary key,
+    text text,
+    forwards jsonb default '[]',
+    images jsonb default '[]',
+    sender_id uuid not null constraint user_id references users(id),
+    chat_id uuid not null constraint chat_id references chats(id)
 );
